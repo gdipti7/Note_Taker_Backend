4fddb7130a9d30a009c6767ccf0d9fe616156683
gdipti7 <diptigiri443@gmail.com>
2026-07-14 11:54:34 +0545

    Setup Express app and server entry point

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
-import dotenv from 'dotenv';
-import app from './src/app.js';
-
+import dotenv from "dotenv";
 dotenv.config();
+
+import app from "./src/app.js";
+import connectDB from "./src/config/db.js";
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+const start = async () => {
+  try {
+    await connectDB();
+
+    app.listen(PORT, () => {
+      console.log(`Server running on http://localhost:${PORT}`);
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+start();
--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
-import express from 'express';
-import cors from 'cors';
-import noteRoutes from './routes/noteRoutes.js';
+import express from 'express'
+import cors from 'cors'
+import noteRoutes from './routes/noteRoutes.js'
 
-const app = express();
+const app = express()
 
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
 
-app.use('/api/notes', noteRoutes);
+app.use('/api/notes', noteRoutes)
 
-export default app;
+export default app
