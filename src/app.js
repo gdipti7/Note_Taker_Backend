2c2bdccc6473346c691760edbfde7974b2943a19
gdipti7 <diptigiri443@gmail.com>
2026-07-21 12:02:03 +0545

    Register auth routes in app.js

--- src/app.js
+++ src/app.js
@@ -1,12 +1,15 @@
 import express from 'express'
 import cors from 'cors'
 import noteRoutes from './routes/noteRoutes.js'
+import authRoutes from './routes/authRoutes.js'
+import verifyToken from './middleware/verifyToken.js'
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 
-app.use('/api/notes', noteRoutes)
+app.use('/auth', authRoutes)
+app.use('/api/notes', verifyToken, noteRoutes)
 
 export default app
