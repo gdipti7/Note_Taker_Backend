9eeb3610a48a2161c6ccc74c74a8c9ec132e80cf
gdipti7 <diptigiri443@gmail.com>
2026-07-26 22:44:27 +0545

    Remove verifyToken from app-level middleware to allow public GET notes route

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ import express from 'express'
 import cors from 'cors'
 import noteRoutes from './routes/noteRoutes.js'
 import authRoutes from './routes/authRoutes.js'
-import verifyToken from './middleware/verifyToken.js'
 
 const app = express()
 
@@ -10,6 +9,6 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/auth', authRoutes)
-app.use('/api/notes', verifyToken, noteRoutes)
+app.use('/api/notes', noteRoutes)
 
 export default app
